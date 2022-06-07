3ea079a77c4a5aab2e9de7cae1c3cea1efc9aa3d
Michael Norris <44741162+NotStanley4330@users.noreply.github.com>
2022-06-07 16:17:53 -0600

    Implemented breakoutDisplay Functions
    created functions for drawing the paddle, ball, tiles, and score. Also Made a function to draw and erase the rows of tiles for the game.

--- lab7/breakoutDisplay.c
+++ lab7/breakoutDisplay.c
@@ -37,20 +37,49 @@ We only care which side they press on to move the paddle left and right, not any
 #define BREAKOUT_DISPLAY_ERASE 1
 
 //size definitions for everything
-#define PADDLE_WIDTH 16
-#define PADDLE_HEIGHT 3
-#define BALL_WIDTH_HEIGHT 4
-#define TILE_WIDTH 8
-#define TILE_HEIGHT 3
+#define PADDLE_WIDTH 64
+#define PADDLE_HEIGHT 6
+#define BALL_WIDTH_HEIGHT 8
+#define TILE_WIDTH 32
+#define TILE_HEIGHT 6
+#define TILE_SPACER_WIDTH 3 //this is a little space between the tiles
 
 
 //starting coordinates for the paddle
 #define PADDLE_INIT_X_COORD ((DISPLAY_WIDTH / SCREEN_REGION_DIVIDER) - (PADDLE_WIDTH / 2))
-#define PADDLE_INIT_Y_COORD (DISPLAY_HEIGHT - 3)
+#define PADDLE_INIT_Y_COORD (DISPLAY_HEIGHT - 10)
 
 //starting coords for the ball
-#define BALL_INIT_X_COORD (DISPLAY_WIDTH - (BALL_WIDTH_HEIGHT / 2))
-#define BALL_INIT_Y_COORD (PADDLE_INIT_Y_COORD - BALL_INIT_X_COORD -1)
+#define BALL_INIT_X_COORD ((DISPLAY_WIDTH / SCREEN_REGION_DIVIDER))
+#define BALL_INIT_Y_COORD (PADDLE_INIT_Y_COORD - 10)
+
+//first cords we can draw a tile at
+#define TILE_FIRST_X_COORD 6
+#define TILE_FIRST_Y_COORD 16
+
+//Score display locations
+#define SCORE_TEXT_X_COORD ((DISPLAY_WIDTH / SCREEN_REGION_DIVIDER) + 64)
+#define SCORE_TEXT_Y_COORD 3
+#define SCORE_X_COORD (SCORE_TEXT_X_COORD + 40)
+#define SCORE_Y_COORD SCORE_TEXT_Y_COORD
+
+//score string
+#define SCORE_STRING "SCORE: "
+
+//color number deifinitons
+#define COLOR_NUM_BLUE 0
+#define COLOR_NUM_RED 1
+#define COLOR_NUM_YELLOW 2
+#define COLOR_NUM_GREEN 4
+#define COLOR_NUM_ORANGE 5
+#define COLOR_NUM_PURPLE 6
+#define COLOR_NUM_CYAN 7
+
+//custom color def
+#define DISPLAY_ORANGE 0xFBA0
+#define DISPLAY_PURPLE 0x8155
+#define DISPLAY_TEAL 0x0737
+
 
 struct ballProperties
 {
@@ -58,10 +87,27 @@ struct ballProperties
     uint8_t yVelocity;
     uint8_t xPosition;
     uint8_t yPosition;
+};
+
+
+
+
+
+
+
+void breakoutDisplay_drawScore(uint32_t score)
+{
+    char scoreString[20] = "";
+    
+    display_setCursor(SCORE_TEXT_X_COORD, SCORE_TEXT_Y_COORD);
+    display_setTextColor(DISPLAY_WHITE);
+    display_setTextSize(1);
+    display_println(SCORE_STRING);
+    display_setCursor(SCORE_X_COORD, SCORE_Y_COORD);
+    sprintf(scoreString, "%i", score);
+    display_println(scoreString);
+
 }
-
-
-
 
 //this functiont akes the coordinates and computes the region of the screen that was touched
 int8_t breakoutDisplay_computeRegionNumber(int16_t x, int16_t y)
@@ -74,31 +120,116 @@ int8_t breakoutDisplay_computeRegionNumber(int16_t x, int16_t y)
     }
     else//otherwise the the touch was on the right side
     {
-            //give us the right reigon number
-            return BREAKOUT_DISPLAY_REGION_1
+        //give us the right reigon number
+        return BREAKOUT_DISPLAY_REGION_1;
+    }
+}
+
+//this function takes a number and returns a 16 bit color code based on the given number
+uint16_t breakoutDisplay_assignColorCode(uint8_t colorNum)
+{
+    //switch statment to take the given integer and return a color based on that
+    switch(colorNum)
+    {
+        case COLOR_NUM_BLUE:
+            return DISPLAY_BLUE;
+            break;
+        case  COLOR_NUM_RED:
+            return DISPLAY_RED;
+            break;
+        case COLOR_NUM_YELLOW:
+            return DISPLAY_YELLOW;
+            break;
+        case COLOR_NUM_GREEN:
+            return DISPLAY_GREEN;
+            break;
+        case COLOR_NUM_ORANGE:
+            return DISPLAY_ORANGE;
+            break;
+        case COLOR_NUM_PURPLE:
+            return DISPLAY_PURPLE;
+            break;
+        case COLOR_NUM_CYAN:
+            return DISPLAY_CYAN;
+            break;
     }
 }
 
 // Draws a colored "button" that the user can touch.
 // The colored button is centered in the region but does not fill the region.
 // If erase argument is true, draws the button as black background to erase it.
-void breakoutDisplay_drawPaddle(uint16_t xCoord, uint16_t yCoord, bool erase)
+void breakoutDisplay_drawPaddle(int16_t xCoord, int16_t yCoord, bool erase)
 {
-    display_fillRect(xCoord, yCoord, PADDLE_WIDTH, PADDLE_HEIGHT, DISPLAY_WHITE);
+    //if we are not erasing it
+    if(!erase)
+    {
+        //draw the paddle in white
+        display_fillRect(xCoord, yCoord, PADDLE_WIDTH, PADDLE_HEIGHT, DISPLAY_TEAL);
+    }
+    else//if we are erasing
+    {
+        //get rid of the thang
+        display_fillRect(xCoord, yCoord, PADDLE_WIDTH, PADDLE_HEIGHT, DISPLAY_BLACK);
+    }
 }
 
 //a function that draws an indivicual tile for use in the game
 //it takes in the x and y coordinates and a number of the color for 
-void breakoutDisplay_drawTile(uint16_t xCoord, uint16_t yCoord, uint32_t colorCode, bool erase)
+void breakoutDisplay_drawTile(int16_t xCoord, int16_t yCoord, uint16_t colorCode, bool erase)
 {
-
+    //if we are not erasing it
+    if(!erase)
+    {
+        //draw that thing
+        display_fillRect(xCoord, yCoord, TILE_WIDTH, TILE_HEIGHT, colorCode);
+    }
+    else//if we are
+    {
+        display_fillRect(xCoord, yCoord, TILE_WIDTH, TILE_HEIGHT, colorCode);
+    }
 }
 
 // a fucntion that draws up the tiles for the start of the level.
-void breakoutDisplay_drawNewTiles();
+void breakoutDisplay_drawNewTiles()
+{
+   
+    int16_t currentXCoord = TILE_FIRST_X_COORD;
+    int16_t currentYCoord = TILE_FIRST_Y_COORD;
+    uint8_t colorNum = 0;
+    uint16_t colorCode = breakoutDisplay_assignColorCode(colorNum);
+
+    //this loop iterates through each row of tiles to be drawn
+    for (int i = 0; i < COLOR_NUM_CYAN + 1; i++)
+    {
+        colorCode = breakoutDisplay_assignColorCode(colorNum);//set the current row color
+        //draw the current row
+        for (int j = 0; j < 9; j++)
+        {
+            breakoutDisplay_drawTile(currentXCoord, currentYCoord, colorCode, 0);
+            currentXCoord += (TILE_WIDTH + TILE_SPACER_WIDTH);
+        }
+        currentXCoord = TILE_FIRST_X_COORD;
+        currentYCoord += (TILE_HEIGHT + TILE_SPACER_WIDTH);
+        colorNum++;
+    }
+}
 
 // Convenience function that erases the entire tile area.
-void breakoutDisplay_eraseAllTiles();
+void breakoutDisplay_eraseAllTiles()
+{
+    //just fill the top half of the screen in black
+    display_fillRect(0, 0, DISPLAY_WIDTH, (DISPLAY_HEIGHT / 2), DISPLAY_BLACK);
+}
 
 //this fucntion is to draw the ball that will be boucnign around the s
-void breakoutDisplay_drawBall(uint16_t xCoord, uint16_t yCoord, bool erase);
+void breakoutDisplay_drawBall(int16_t xCoord, int16_t yCoord, bool erase)
+{
+    if (!erase)//of we arent erasing it draw that thang
+    {
+        display_fillCircle(xCoord, yCoord, BALL_WIDTH_HEIGHT, DISPLAY_WHITE);
+    }
+    else//we needa erase this thing now
+    {
+        display_fillCircle(xCoord, yCoord, BALL_WIDTH_HEIGHT, DISPLAY_BLACK);
+    }
+}
--- lab7/breakoutDisplay.h
+++ lab7/breakoutDisplay.h
@@ -34,19 +34,63 @@ We only care which side they press on to move the paddle left and right, not any
 #define BREAKOUT_DISPLAY_DRAW 0
 #define BREAKOUT_DISPLAY_ERASE 1
 
+//size definitions for everything
+#define PADDLE_WIDTH 64
+#define PADDLE_HEIGHT 6
+#define BALL_WIDTH_HEIGHT 8
+#define TILE_WIDTH 32
+#define TILE_HEIGHT 6
+
+
+//starting coordinates for the paddle
+#define PADDLE_INIT_X_COORD ((DISPLAY_WIDTH / SCREEN_REGION_DIVIDER) - (PADDLE_WIDTH / 2))
+#define PADDLE_INIT_Y_COORD (DISPLAY_HEIGHT - 10)
+
+//starting coords for the ball
+#define BALL_INIT_X_COORD ((DISPLAY_WIDTH / SCREEN_REGION_DIVIDER))
+#define BALL_INIT_Y_COORD (PADDLE_INIT_Y_COORD - 10)
+
+//Score display locations
+#define SCORE_TEXT_X_COORD ((DISPLAY_WIDTH / SCREEN_REGION_DIVIDER) + 64)
+#define SCORE_TEXT_Y_COORD 3
+#define SCORE_X_COORD (SCORE_TEXT_X_COORD + 40)
+#define SCORE_Y_COORD SCORE_TEXT_Y_COORD
+
+//first cords we can draw a tile at
+#define TILE_FIRST_X_COORD 6
+#define TILE_FIRST_Y_COORD 16
+
+//color number deifinitons
+#define COLOR_NUM_BLUE 0
+#define COLOR_NUM_RED 1
+#define COLOR_NUM_YELLOW 2
+#define COLOR_NUM_GREEN 4
+#define COLOR_NUM_ORANGE 5
+#define COLOR_NUM_PURPLE 6
+#define COLOR_NUM_CYAN 7
+
+//custom color defr
+#define DISPLAY_ORANGE 0xFBA0
+#define DISPLAY_TEAL 0x0737
+#define DISPLAY_PURPLE 0x8155
+
 
 
 
 int8_t breakoutDisplay_computeRegionNumber(int16_t x, int16_t y);
 
+uint16_t breakoutDisplay_assignColorCode(uint8_t colorNum);
+
+void breakoutDisplay_drawScore(uint32_t score);
+
 // Draws a colored "button" that the user can touch.
 // The colored button is centered in the region but does not fill the region.
 // If erase argument is true, draws the button as black background to erase it.
-void breakoutDisplay_drawPaddle(uint8_t regionNumber, bool erase);
+void breakoutDisplay_drawPaddle(int16_t xCoord, int16_t yCoord, bool erase);
 
 //a function that draws an indivicual tile for use in the game
 //it takes in the x and y coordinates and a number of the color for 
-void breakoutDisplay_drawTile(uint16_t xCoord, uint16_t yCoord, uint32_t colorCode)
+void breakoutDisplay_drawTile(int16_t xCoord, int16_t yCoord, uint16_t colorCode, bool erase);
 
 // a fucntion that draws up the tiles for the start of the level.
 void breakoutDisplay_drawNewTiles();
@@ -55,7 +99,7 @@ void breakoutDisplay_drawNewTiles();
 void breakoutDisplay_eraseAllTiles();
 
 //this fucntion is to draw the ball that will be boucnign around the s
-void breakoutDisplay_drawBall(uint16_t xCoord, uint16_t yCoord, bool erase);
+void breakoutDisplay_drawBall(int16_t xCoord, int16_t yCoord, bool erase);
 
 
 
